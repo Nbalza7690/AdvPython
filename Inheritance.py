07969d9c2098ca93e431b81a82eef1abd08d1460
NBalza <Nbalza7690@gmail.com>
2020-10-01 19:34:09 -0700

    Create Inheritance.py
    Added inheritance assignment

---
+++ Inheritance.py
@@ -0,0 +1,17 @@
+
+# Parent Class
+class Employee:
+    name = ""
+    empid = 0
+    email = ""
+    phone = 0
+    base_pay = 0
+
+# Child Classes
+class Cook(Employee):
+    numb_of_Knives = 4
+    breaks_allowed = False
+
+class Bartender(Employee)
+    uniform = 'Rustic Apron'
+    pos_access = True
